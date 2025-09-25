864e1fc7e64019057268ba5d5ca6d89fe7e41d52
abelmoha <Pin2picee@exemple.com>
2025-09-25 09:25:37 +0200

    update de la branche ::
    chaque branche features possede seulement sa fonctionnalite et ne porte pas tout le projet avec elle

--- includes/Socket.hpp
+++ includes/Socket.hpp
@@ -14,15 +14,31 @@
 
 # define SOCKET_HPP
 
-# include <sys/socket.h>
-// #class socket va creer un socket 
+#include <sys/socket.h>   // pour socket(), bind(), sockaddr et macro
+#include <netinet/in.h>   // pour sockaddr_in, in_addr
+# include <iostream>
+# include <errno.h>
+# include <string.h>
+
+// #class qui va creer un socket 
 class Socket
 {
     private :
-        int Fd;
+        int Fd;// file descriptor genere par socket()
+        struct sockaddr_in address1;// structure pour paramtrer l'adrresse a bind
+        size_t _port;
+        
+    private :
+        Socket();//lance un socket
+        void    set_socket_addr();//methode qui definie les valeurs a implement
     public:
-        Socket();
-        ~Socket();
-        getFd();
+        Socket(size_t port);
+        ~Socket();// le ferme
+        int getFd(void) const;// recupere le 
+
+    class   SocketError : public std::exception
+    {
+        const char *what(void) const throw ();
+    };
 };
 #endif
--- src/Socket.cpp
+++ src/Socket.cpp
@@ -12,8 +12,32 @@
 
 #include "../includes/Socket.hpp"
 
-Socket::Socket()
+/*EXCEPTION*/
+const char *Socket::SocketError::what() const throw()
 {
-    this->Fd = socket(AF_INET);
-    this->
+    return (strerror(errno));
+}
+Socket::Socket() {};
+
+
+Socket::Socket(size_t port)
+{
+    this->_port = port;
+    //creation du socket directement dans le constructeur a l'aide de la fonction socket
+    //param : 1=adressIPV4, 2= TCP, 3=default TCP/ip
+    this->Fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (Fd < 0)
+        throw SocketError();
+    set_socket_addr();
+    if (bind(this->Fd, (const sockaddr *)&this->address1, sizeof(address1)) != 0)
+        throw SocketError();
+    //on lie le socket a une adress ip
+}
+
+void    Socket::set_socket_addr()
+{
+    this->address1.sin_family = AF_INET;// IPV4
+    this->address1.sin_port = htons(this->_port);
+    this->address1.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1 // little_endian to big_endian for network // inet_pton mais pas le droit
+
 }
