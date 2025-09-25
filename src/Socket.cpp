af57dc1196b97f7e34067844dde79cbf3198ace5
abelmoha <Pin2picee@gmail.com>
2025-09-25 10:43:21 +0200

    ADD function listen for socket
    now all socket listen port in param

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -12,32 +12,40 @@
 
 #include "../includes/Socket.hpp"
 
-/*EXCEPTION*/
+/*<EXCEPTION>*/
 const char *Socket::SocketError::what() const throw()
 {
     return (strerror(errno));
 }
+/*</EXCEPTION>*/
+
+
+/*<CONSTRUCTION>*/
 Socket::Socket() {};
 
-
+/**
+ * @brief = creation du socket + liaison(=bind) localhost + port
+ * @param = port sur lequel on va listen
+ */
 Socket::Socket(size_t port)
 {
     this->_port = port;
-    //creation du socket directement dans le constructeur a l'aide de la fonction socket
-    //param : 1=adressIPV4, 2= TCP, 3=default TCP/ip
     this->Fd = socket(AF_INET, SOCK_STREAM, 0);
     if (Fd < 0)
         throw SocketError();
-    set_socket_addr();
+    set_socket_addr();// initialisation de la structure sock_addr_in puis convertit en (const sock_adrr *)
     if (bind(this->Fd, (const sockaddr *)&this->address1, sizeof(address1)) != 0)
         throw SocketError();
-    //on lie le socket a une adress ip
+    else if (listen(this->Fd, 10000) < 0)// 2emeparam= backlog file d'attente dont la connexion n'est pas encore accepter
+        throw SocketError();
 }
+/*</CONSTRUCTION>*/
 
+/**/
 void    Socket::set_socket_addr()
 {
-    this->address1.sin_family = AF_INET;// IPV4
+    this->address1.sin_family = AF_INET;
     this->address1.sin_port = htons(this->_port);
-    this->address1.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1 // little_endian to big_endian for network // inet_pton mais pas le droit
+    this->address1.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1 // little_endian to big_endian for network // inet_pton->function mais pas le droit
 
 }
