6b97f8a4290183831e72e86f249d91ddc81f2bfa
abelmoha <Pin2picee@gmail.com>
2025-10-01 15:26:45 +0200

    update my features for deconnection, now i erase fd and object in my map if user is not connected
    my map is use for fd connection
    
    and i update logic of my boucle for : i is increment in condition if deconnected i don't increment

--- includes/Monitor.hpp
+++ includes/Monitor.hpp
@@ -6,7 +6,7 @@
 /*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 10:48:18 by abelmoha@st       #+#    #+#             */
-/*   Updated: 2025/09/27 17:30:55 by abelmoha         ###   ########.fr       */
+/*   Updated: 2025/10/01 14:23:31 by abelmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,14 @@ class Monitor
 		size_t				nb_socket;
 		size_t				nb_socket_server;
 		std::map<int, Client>	clients;
+		std::vector<Client>		log_clients;
 		struct  pollfd		all_socket[20000];// dois tout avoir dans ce meme tab connexion for read & write
 	private :
 		Monitor();
 		void	add_client(int fd, in_addr_t ip, in_port_t port);// ajoute un client  dans mon vecteur de client
 		int		new_clients(int i);//ajoute un nouveau socket non bloquant pour le client
 		int		new_request(int i);// RECUP la requete + verif test_read
-		int		test_read(int i, ssize_t count);
+		int		test_read(ssize_t count);
 		int		deconnexion(int i);//SUPPRIME LA CONNEXION SOCKET CLIENT IF !TEST_READ
 	public :
 		Monitor(std::vector<int> tab);
--- src/Monitor.cpp
+++ src/Monitor.cpp
@@ -6,7 +6,7 @@
 /*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 11:12:02 by abelmoha          #+#    #+#             */
-/*   Updated: 2025/09/27 17:10:37 by abelmoha         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:22:58 by abelmoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,8 @@ Monitor::Monitor(std::vector<int> tab)
 /*AJOUT DU CLIENT DANS LA MAP FD:CLIENT*/
 void Monitor::add_client(int fd, in_addr_t ip, in_port_t port)
 {
+	std::cout << "fd = " << fd << ", map size = " << clients.size() << std::endl;
+	std::cout << "\033[31m" <<"nombre d'entre dans add_client" << "\033[0m"<< std::endl;
 	Client  nouveau;
 	uint32_t    ip_adress = ntohl(ip);
 	uint16_t    port_adress = ntohs(port);
@@ -107,25 +109,26 @@ int     Monitor::deconnexion(int i)
 	 */
 	clients[all_socket[i].fd].deconected();
 	close(all_socket[i].fd);
+	clients.erase(all_socket[i].fd);
 	all_socket[i] = all_socket[nb_socket - 1];
 	all_socket[nb_socket - 1].fd = -1;//poll ne le surveillera plus
 	nb_socket--;
 	return (0);
 }
 
-int     Monitor::test_read(int i, ssize_t count)
+int     Monitor::test_read(ssize_t count)
 {
 	if (count > 0)
 		return (1);
 	else if (count < 0)//rien a lire pour l'instant ou deco
 	{
 		if (errno == EAGAIN || errno == EWOULDBLOCK)// r a lire
-			return (0);
+			return (2);
 		else// alors deconnexion
-			return (deconnexion(i));
+			return (0);
 	}
 	else//(count == 0)//deco
-		return (deconnexion(i));
+		return (0);
 }
 
 int		Monitor::new_request(int i)
@@ -133,16 +136,20 @@ int		Monitor::new_request(int i)
 	ssize_t count;
 	char buf[1024];
 	std::string buf_final;
+	int	result;
 	
 	while (42)
 	{
 		count = read(all_socket[i].fd, buf, sizeof(buf));
-		if (!test_read(i, count))//client deco
+		result = test_read(count);
+		if (!result)//client deco
+			return (deconnexion(i));
+		else if (result == 2)
 			break;
 		buf_final.append(buf, count);
 	}
 	clients[all_socket[i].fd].setRequest(buf_final);
-	return (0);
+	return (1);
 }
 
 int	Monitor::new_clients(int i)
@@ -151,10 +158,12 @@ int	Monitor::new_clients(int i)
 	socklen_t addrlen = sizeof(address);
 	all_socket[nb_socket].fd = accept(all_socket[i].fd, (sockaddr *)&address, &addrlen);
 	all_socket[nb_socket].events = POLLIN | POLLOUT;
+	if (all_socket[nb_socket].fd < 0 && (errno == EAGAIN || errno == EWOULDBLOCK))
+		return (std::cout << "RIEN A ACCEPTER"<< std::endl, 1) ;
+	else if (all_socket[nb_socket].fd < 0)
+		return (std::cout << "ERROR ressources accept" << std::endl, 1);
 	add_client(all_socket[nb_socket].fd, address.sin_addr.s_addr, address.sin_port);// on l'ajoute dans ma map de clients
-	if (all_socket[nb_socket].fd < 0)
-		return (std::cout << "ACCEPTE ECHOUE"<< std::endl, 1) ;
-		
+	
 	int ancien_flags = fcntl (all_socket[nb_socket].fd, F_GETFL);
 	fcntl(all_socket[nb_socket].fd, F_SETFL, ancien_flags | O_NONBLOCK);
 	nb_socket++;
@@ -171,6 +180,9 @@ void    Monitor::Monitoring()
 
 	while (42)
 	{
+		std::cout << "La taille du nombre de client vector_client : "<< this->clients.size() << std::endl;
+		std::cout << "nb_socket_server : "<< this->nb_socket_server << std::endl;
+		std::cout << "nb_socket_total : "<< this->nb_socket<< std::endl;
 		poll_return = poll(this->all_socket, nb_socket, 15);
 		if (poll_return == 0)//AUCUN SOCKET du TAB n'est pret timeout
 			continue ;
@@ -178,14 +190,17 @@ void    Monitor::Monitoring()
 			throw MonitorError();
 		else if (poll_return > 0)//un ou plusieurs socket pret
 		{
-			for (int i = 0; i < nb_socket; i++)//parcours les socket
+			for (int i = 0; i < nb_socket;)//parcours les socket
 			{
 				if (all_socket[i].revents & POLLIN)// read ready
 				{
-					if (i < nb_socket_server && new_clients(i))// nouvelle connexion client a accept
-							continue;
-					else if (i >= nb_socket_server && new_request(i))//client
-							perror("Error : Accept()\n");
+					if (i < nb_socket_server && new_clients(i))//true == accept echoue
+					{
+						i++;
+						continue;
+					}
+					else if (i >= nb_socket_server && !new_request(i))//client/deconnexion
+							continue;// on reviens sur le meme i car il a etait changer par le dernier dans  deconnexion
 				}
 				if (all_socket[i].revents & POLLOUT && i >= nb_socket_server)//cote client je peux ecrire
 				{
@@ -197,6 +212,7 @@ void    Monitor::Monitoring()
 					if (nb_send <= 0)
 						perror("ERROR : SEND FAILED \n");
 				}
+				i++;
 			}
 		}
 	}
