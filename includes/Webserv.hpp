e35a338ee438c5ace19dd92b4dd64beb1303fab6
abelmoha <Pin2picee@exemple.com>
2025-10-15 16:49:25 +0200

    update include Webserv.hpp

--- includes/Webserv.hpp
+++ includes/Webserv.hpp
@@ -1,24 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelmoha <abelmoha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 15:04:35 by abelmoha          #+#    #+#             */
+/*   Updated: 2025/10/15 15:09:55 by abelmoha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef WEBSERV_HPP
 #define WEBSERV_HPP
 
-#include <unistd.h>        // fork, execve, pipe, dup, dup2, _exit
-#include <sys/wait.h>      // waitpid
-#include <signal.h>        // kill, signal
-#include <sys/types.h>     //
-#include <sys/time.h>      //
-#include <sys/socket.h>    // socket, bind, listen, accept, connect, getsockname, setsockopt, recv, send, socketpair
-#include <netdb.h>         // getaddrinfo, freeaddrinfo, getprotobyname, gai_strerror
-#include <arpa/inet.h>     // htons, htonl, ntohs, ntohl
-#include <netinet/in.h>    // struct sockaddr_in
-#include <poll.h>          // poll
-#include <sys/select.h>    // select
-#include <sys/epoll.h>     // epoll
-#include <sys/stat.h>      // stat
-#include <fcntl.h>         // open, fcntl
-#include <dirent.h>        // opendir, readdir, closedir
-#include <string.h>        // strerror
-#include <errno.h>         // errno
+/*
+==========================
+    LIB FOR WEBSERV
+    BY abelmoha & locagnio
+==========================
+*/
 
-#include <iostream>
+# include <csignal>
+# include <errno.h>
+# include <fcntl.h>
+# include <iostream>
+# include <map>
+# include <netinet/in.h>
+# include <poll.h>
+# include <sstream>
+# include <string.h>
+# include <string>
+# include <sys/socket.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <vector>
+# include "Locations.hpp"
 
 #endif
