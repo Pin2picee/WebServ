f93caca259e670772a4a77568682f7221c1b21bc
Sung Jinwoo <codes_of_war@Baki.>
2025-11-05 22:22:04 +0100

    merge cgi

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -7,19 +7,24 @@
  * @brief
  * The class that respond to `Requests`.
  * 
- * @param _server				The `Server` that sends a `Request`.
+ * @param _server	The `Server` that sends a `Request`.
  */
 class ResponseHandler
 {
 	private:
 		const Server	&_server;
 
-		Response		handleGet(const Locations &loc, const Request &req);
-		Response		handlePost(const Locations &loc, const Request &req);
-		Response		handleDelete(const Locations &loc, const Request &req);
-	public:
-		ResponseHandler(const Server &server);
-		~ResponseHandler();
+	//methods
+	Response		handleGet(const Locations &loc, const Request &req);
+	Response		handlePost(const Locations &loc, const Request &req);
+	Response		handleDelete(const Locations &loc, const Request &req);
+
+	//utils
+	std::string		requestToString(const Request &req);
+	std::string		responseToString(const Response &res);
+public:
+	ResponseHandler(const Server &server);
+	~ResponseHandler();
 
 	//handle requests
 	Response		handleRequest(const Request &req);
