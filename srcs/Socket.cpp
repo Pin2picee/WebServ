25ca895e5524b96c66b7180652d3b521bbc71979
Louis Cagnion <locagnio@c3r3p1.42perpignan.fr>
2026-01-14 19:30:55 +0100

    Fix:Socket fcntl return -1 beacause forgot Flags SETFL

--- srcs/Socket.cpp
+++ srcs/Socket.cpp
@@ -61,7 +61,7 @@ Socket::Socket(std::string ip, int port, Server *refBlock)
 	else if (listen(this->Fd, SOMAXCONN) < 0)// 2emeparam= backlog file d'attente dont la connexion n'est pas encore accepter
 		throw SocketError();
 	int ancien_flags = fcntl(this->Fd, F_GETFL);
-	int res = fcntl(this->Fd, ancien_flags | O_NONBLOCK);
+	int res = fcntl(this->Fd, F_SETFL, ancien_flags | O_NONBLOCK);
 	if (ancien_flags == -1 || res == -1)
 		throw SocketError();
 }
